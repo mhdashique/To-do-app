288c53405cf2d1d9bcf0092c379d3a06bb477fd5
ashique <pepeashique@gmail.com>
2025-02-12 02:34:01 +0530

    setup title, todolist and todoitems design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Todo from './components/Todo'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='bg-gray-500'>to do</h1>
+    <div className='bg-stone-800 grid py-4 min-h-screen'>
+      <Todo/>
     </div>
   )
 }
