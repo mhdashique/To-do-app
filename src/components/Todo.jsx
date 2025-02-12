98ad2f578edbfec5cef98e90b70889c5ab08c988
ashique <pepeashique@gmail.com>
2025-02-12 19:52:14 +0530

    add new task in To-Do app

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Todoitems from "./Todoitems";
 import todo_icon from '../assets/todo_icon.png'
 
 const Todo = () => {
+  const [todolist,setTodoList] = useState([])
+  const inputRef = useRef();
+  const add = ()=>{
+    const inputText = inputRef.current.value.trim();
+
+    if(inputText === ""){
+        return null;
+    }
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    }
+    setTodoList((prev)=>[...prev,newTodo]);
+    inputRef.current.value = "";
+  }
+
   return (
     <div
       className="bg-white place-self-center w-11/12 max-w-md 
@@ -16,15 +33,17 @@ const Todo = () => {
 
     {/* ----input box--- */}
     <div className="flex items-center my-7 bg-gray-200 rounded-full">
-        <input className="bg-transparent border-0 outline-none
+        <input ref={inputRef}  className="bg-transparent border-0 outline-none
         flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600 " type="text" placeholder="Add Your Task"/>
-        <button className="border-none rounded-full bg-orange-700 w-32 h-14 text-white
+        <button onClick={add} className="border-none rounded-full bg-orange-700 w-32 h-14 text-white
         text-lg fond-medium cursor-pointer">ADD+</button>
     </div>
 
     {/* ---todo list--- */}
     <div>
-        <Todoitems/> 
+      {todolist.map((item,index)=>{
+       return <Todoitems key={index} text={item.text} />    
+      })} 
     </div>
 
     </div>
--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -3,13 +3,14 @@ import tick from "../assets/tick.png";
 import not_tick from "../assets/not_tick.png";
 import delete_icon from "../assets/delete.png";
 
-const Todoitems = () => {
+const Todoitems = ({text}) => {
   return (
     <div className="flex items-center my-3 gap-2 ">
-      <div>
-        <img src={tick} alt="" />
-        <p>Learing coding</p>
+      <div className="flex flex-1 items-center cursor-pointer">
+        <img className="w-7" src={tick} alt="" />
+        <p className="text-slate-700 ml-4 text-[17px]">{text}</p>
       </div>
+      <img className="w-3.5" src={delete_icon} alt="" />
     </div>
   );
 };
